8540369675d4d1641a5fc6f4982b9ee71106d3d9
jmmmnm <jmmmnm@gmail.com>
2019-10-07 00:01:19 +0900

    カリキュラムの遷移まで

--- app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
@@ -2,11 +2,21 @@ package jp.techacademy.minami.jommo.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
