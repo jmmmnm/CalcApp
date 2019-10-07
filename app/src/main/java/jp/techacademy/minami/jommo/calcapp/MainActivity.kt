162459241560cdfe1aa30f9a7de083b124c110d4
jmmmnm <jmmmnm@gmail.com>
2019-10-07 23:05:41 +0900

    それぞれのボタンが働くように

--- app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.techacademy.minami.jommo.calcapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 
@@ -18,9 +19,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button4.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+  //  var num1: String = editText1.text.toString()
+
+
+  //  var num2: String = editText2.text.toString()
+
+
+    override fun onClick(v: View) {
+
+        if (v.id == R.id.button1) {
+            Log.d("CALC_TEST","${editText1.text}+${editText2.text}")
+        } else if (v.id == R.id.button2) {
+            Log.d("CALC_TEST","${editText1.text}-${editText2.text}")
+        } else if (v.id == R.id.button3) {
+            Log.d("CALC_TEST","${editText1.text}*${editText2.text}")
+        } else if (v.id == R.id.button4) {
+            Log.d("CALC_TEST","${editText1.text}/${editText2.text}")
+        }
     }
+
 
 }
