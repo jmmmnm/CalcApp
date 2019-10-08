74884786fd769c8c312b3eaf2d5a200ae5a161a3
jmmmnm <jmmmnm@gmail.com>
2019-10-08 21:41:56 +0900

    計算できるか

--- app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/minami/jommo/calcapp/MainActivity.kt
@@ -23,21 +23,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         var nyuryoku1: String = editText1.text.toString()
         var nyuryoku2: String = editText2.text.toString()
+        var kigo: String = "%"
 
         if(nyuryoku1 == "" || nyuryoku2 == ""){
             Log.d("CALC_TEST","ダメダメだよ")
-        }else if (v.id == R.id.button1) {
-            Log.d("CALC_TEST","${editText1.text}+${editText2.text}")
-        } else if (v.id == R.id.button2) {
-            Log.d("CALC_TEST","${editText1.text}-${editText2.text}")
-        } else if (v.id == R.id.button3) {
-            Log.d("CALC_TEST","${editText1.text}*${editText2.text}")
-        } else if (v.id == R.id.button4) {
-            Log.d("CALC_TEST","${editText1.text}/${editText2.text}")
+        }else{
+            if (v.id == R.id.button1){kigo = "+"
+            }else if (v.id == R.id.button2){kigo = "-"
+            }else if (v.id == R.id.button3){kigo = "*"
+            }else if (v.id == R.id.button4){kigo = "/"
+            }
         }
 
+        Log.d("CALC_TEST","${kigo}")
 
+        var n1: Double = nyuryoku1.toDouble()
+        var n2: Double = nyuryoku2.toDouble()
+
+        Log.d("CALC_TEST","${n1}${kigo}${n2}")
+
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", n1)
+        intent.putExtra("VALUE2", n2)
+        startActivity(intent)
     }
-
-
 }
+
+
+
+
+
+
+
--- app/src/main/java/jp/techacademy/minami/jommo/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/minami/jommo/calcapp/SecondActivity.kt
@@ -2,11 +2,18 @@ package jp.techacademy.minami.jommo.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val value1 = intent.getDoubleExtra("VALUE1", 0.0)
+        val value2 = intent.getDoubleExtra("VALUE2", 0.0)
+
+        textView.text = "${value1 + value2}"
+
     }
 }
